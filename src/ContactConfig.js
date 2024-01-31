5c0f6f88ff5f00c7fab3602da11669d4612c4f53
eidde606 <eidde_606@yahoo.com>
2024-01-30 20:05:52 -0500

    update

--- src/ContactConfig.js
+++ src/ContactConfig.js
@@ -1,11 +1,11 @@
 const contactConfig = {
-  YOUR_EMAIL: "eiddenazario@gmail.com",
+  YOUR_EMAIL: "andreysprolandscaping@gmail.com",
   description: "Looking forward to hearing from you!",
   // creat an emailjs.com account
   // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-  YOUR_SERVICE_ID: "service_fumnsvn",
-  YOUR_TEMPLATE_ID: "template_bbixkrb",
-  YOUR_USER_ID: "-N8AEob4bCojrOdby",
+  YOUR_SERVICE_ID: "service_bfubwuq",
+  YOUR_TEMPLATE_ID: "template_0si7pol",
+  YOUR_USER_ID: "4aPK-D46Ugv5rD2xr",
 };
 
 export { contactConfig };
